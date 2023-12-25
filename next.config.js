b6a8aec1cc5c11b30afd186b9e91b30d853fda9a
rlotr02 <0314kelee@naver.com>
2023-12-25 13:58:51 +0900

    Feat: svg 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  // svg 사용할 때 <Icon /> 처럼 사용하기 위함
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
