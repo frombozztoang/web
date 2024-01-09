96f4a7dea25a781f6c8d474c545b5b21331ccb46
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-01-10 08:04:19 +0900

    Build: import relocation

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,8 +1,8 @@
 'use client';
+import React, { useState } from 'react';
 import StudyToggle from '@/components/atom/toggle/StudyToggle';
 import Setting from '@/components/templates/mypage/setting';
 import Favorites from '@/components/templates/mypage/favorites';
-import React, { useEffect, useState } from 'react';
 
 import { KAKAO_REDIRECT_URI_DEPLOY, KAKAO_REDIRECT_URI_DEVELOPMENT } from '@/constants/redirectUri';
 
